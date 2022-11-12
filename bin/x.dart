d8392adcbad2be33bb0005a10254ef0a26dd8f1d
HuzeyfeHalil <117576180+HuzeyfeHalil@users.noreply.github.com>
2022-11-12 13:32:43 +0300

    first commit

---
+++ bin/x.dart
@@ -0,0 +1,23 @@
+
+void main() {
+int x=-90;
+ if(x<0) {
+   print ("negative");
+   }
+   else if (x==0){
+     print ("zero");
+ }else  {
+   print ("positve");
+ }String name="huzeyfe";
+if (name=="huzeyyfe"){
+  print("ahmed");
+}else{
+  print ("muhammed");
+  int z=11,h=12,a=7;
+  if (z==11 &&h==12 ||a==7){
+    print ("allthing is right");
+  }else{
+    print ("allthing is false brooooo!");//the all alllthink about meee 
+  }
+}
+}
