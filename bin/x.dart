3ffd6feecea368a6636669915910975436e2ade2
HuzeyfeHalil <117576180+HuzeyfeHalil@users.noreply.github.com>
2022-11-12 14:41:54 +0300

    NNN

--- bin/x.dart
+++ bin/x.dart
@@ -1,27 +1,14 @@
 void main() {
-  int x = -90;
-  if (x < 0) {
-    print("negative");
-  } else if (x == 0) {
-    print("zero");
+  int x = 90;
+  if (x <= 10) {
+    print("my name is the buttle");
   } else {
-    print("positve");
+    print("iam the dark n the light");
   }
-  String name = "huzeyfe";
-  if (name == "huzeyyfe") {
-    print("ahmed");
+  String n1 = "huzeyfe halil0";
+  if (n1 == "huzeyfehalil") {
+    print('IAM SO HAPPPY');
   } else {
-    print("muhammed");
-    int z = 11, h = 12, a = 7;
-    if (z == 11 && h == 12 || a == 7) {
-      print("allthing is right");
-    } else {
-      print("allthing is false brooooo!"); //the all alllthink about meee
-    }
-    var i = "huzeyfe halil muhammid bin caber";
-    print(i);
-    var w = 219829812921928191829.129182;
-    var g = 181818181181818;
-    print('$w $g');
+    print("IAM SO SADDDDD!");
   }
 }
--- lib/x.dart
+++ lib/x.dart
@@ -1,3 +1,14 @@
-int calculate() {
-  return 6 * 7;
+void main() {
+  int x = 90;
+  if (x <= 10) {
+    print("my name is the buttle");
+  } else {
+    print("iam the dark n the light");
+  }
+  String n1 = "huzeyfe halil0";
+  if (n1 == "huzeyfehalil") {
+    print('IAM SO HAPPPY');
+  } else {
+    print("IAM SO SADDDDD!");
+  }
 }
