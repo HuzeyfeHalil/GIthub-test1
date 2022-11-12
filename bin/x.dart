0774f5e4f369296d84339ea15dfb9a545f4808c9
HuzeyfeHalil <117576180+HuzeyfeHalil@users.noreply.github.com>
2022-11-12 14:25:12 +0300

    first commit

--- bin/x.dart
+++ bin/x.dart
@@ -1,23 +1,28 @@
-
 void main() {
-int x=-90;
- if(x<0) {
-   print ("negative");
-   }
-   else if (x==0){
-     print ("zero");
- }else  {
-   print ("positve");
- }String name="huzeyfe";
-if (name=="huzeyyfe"){
-  print("ahmed");
-}else{
-  print ("muhammed");
-  int z=11,h=12,a=7;
-  if (z==11 &&h==12 ||a==7){
-    print ("allthing is right");
-  }else{
-    print ("allthing is false brooooo!");//the all alllthink about meee 
+  int x = -90;
+  if (x < 0) {
+    print("negative");
+  } else if (x == 0) {
+    print("zero");
+  } else {
+    print("positve");
   }
-}
+  String name = "huzeyfe";
+  if (name == "huzeyyfe") {
+    print("ahmed");
+  } else {
+    print("muhammed");
+    int z = 11, h = 12, a = 7;
+    if (z == 11 && h == 12 || a == 7) {
+      print("allthing is right");
+    } else {
+      print("allthing is false brooooo!"); //the all alllthink about meee
+    }
+    var i = "huzeyfe halil muhammid bin caber";
+    print(i);
+    var w = 219829812921928191829.129182;
+    var g = 181818181181818;
+    print('$w $g');
+    print('$w $g');
+  }
 }
